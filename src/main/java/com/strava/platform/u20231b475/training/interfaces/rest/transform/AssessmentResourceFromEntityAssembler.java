ad449da6ab181869d811f72e9978abd475bf4465
Trevor <valentinosolischang@gmail.com>
2025-11-20 11:15:03 -0500

    Refactor AssessmentResourceFromEntityAssembler to improve code clarity and maintainability

--- src/main/java/com/strava/platform/u20231b475/training/interfaces/rest/transform/AssessmentResourceFromEntityAssembler.java
+++ src/main/java/com/strava/platform/u20231b475/training/interfaces/rest/transform/AssessmentResourceFromEntityAssembler.java
@@ -1,5 +1,29 @@
 package com.strava.platform.u20231b475.training.interfaces.rest.transform;
 
-public class AssessmentResourceFromEntityAssembler {
+import com.strava.platform.u20231b475.training.domain.model.aggregates.Assessment;
+import com.strava.platform.u20231b475.training.interfaces.rest.resources.AssessmentResource;
 
+/**
+ * Assembler to convert an Assessment entity to an AssessmentResource.
+ */
+public class AssessmentResourceFromEntityAssembler {
+  /**
+   * Converts an Assessment entity to an AssessmentResource
+   * 
+   * @param entity The {@link Assessment} entity to convert
+   * @return The {@link AssessmentResource} resource
+   */
+  public static AssessmentResource toResourceFromEntity(Assessment entity) {
+    return new AssessmentResource(
+        entity.getId(),
+        entity.getAthleteId(),
+        entity.getCoachId(),
+        entity.getBmi(),
+        entity.getPushUps(),
+        entity.getPlankTime(),
+        entity.getCardioMetrics(),
+        entity.getConfidentialNote(),
+        entity.getType(),
+        entity.getStatus());
+  }
 }
