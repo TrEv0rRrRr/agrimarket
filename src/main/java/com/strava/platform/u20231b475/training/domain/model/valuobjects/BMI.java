019413e453c73a1240e39f7bf3ad8d80d56445c6
Trevor <valentinosolischang@gmail.com>
2025-11-19 23:42:46 -0500

    Add BMI value object with validation constraints

---
+++ src/main/java/com/strava/platform/u20231b475/training/domain/model/valuobjects/BMI.java
@@ -0,0 +1,17 @@
+package com.strava.platform.u20231b475.training.domain.model.valuobjects;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * BMI Value Object
+ * 
+ * @author Valentino Solis
+ */
+@Embeddable
+public record BMI(@NotNull @DecimalMin("10.0") @DecimalMax("40.0") BigDecimal value) {
+}
