0ed00b02e2a1e26d72979de41ff421420a1c3343
Trevor <valentinosolischang@gmail.com>
2025-11-20 10:57:30 -0500

    Refactor GetAssessmentByAthleteIdAndCoachIdAndDateQuery to use value objects for athleteId and coachId

--- src/main/java/com/strava/platform/u20231b475/training/domain/model/queries/GetAssessmentByAthleteIdAndCoachIdAndDateQuery.java
+++ src/main/java/com/strava/platform/u20231b475/training/domain/model/queries/GetAssessmentByAthleteIdAndCoachIdAndDateQuery.java
@@ -2,5 +2,11 @@ package com.strava.platform.u20231b475.training.domain.model.queries;
 
 import java.time.LocalDate;
 
-public record GetAssessmentByAthleteIdAndCoachIdAndDateQuery(Long athleteId, Long coachId, LocalDate date) {
+import com.strava.platform.u20231b475.training.domain.model.valuobjects.AthleteId;
+import com.strava.platform.u20231b475.training.domain.model.valuobjects.CoachId;
+
+/**
+ * Get an assessment by athleteId, coachId and the actual date
+ */
+public record GetAssessmentByAthleteIdAndCoachIdAndDateQuery(AthleteId athleteId, CoachId coachId, LocalDate date) {
 }
