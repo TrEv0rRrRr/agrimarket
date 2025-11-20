a057ad49317f6cddffc667aae12949cc259500f9
Trevor <valentinosolischang@gmail.com>
2025-11-20 11:15:07 -0500

    Add CreateAssessmentCommandFromResourceAssembler for converting CreateAssessmentResource to CreateAssessmentCommand

---
+++ src/main/java/com/strava/platform/u20231b475/training/interfaces/rest/transform/CreateAssessmentCommandFromResourceAssembler.java
@@ -0,0 +1,29 @@
+package com.strava.platform.u20231b475.training.interfaces.rest.transform;
+
+import com.strava.platform.u20231b475.training.domain.model.commands.CreateAssessmentCommand;
+import com.strava.platform.u20231b475.training.interfaces.rest.resources.CreateAssessmentResource;
+
+/**
+ * Assembler to convert a CreateAssessmentResource to a CreateAssessmentCommand.
+ */
+public class CreateAssessmentCommandFromResourceAssembler {
+  /**
+   * Converts a CreateAssessmentResource to a CreateAssessmentCommand
+   * 
+   * @param resource The {@link CreateAssessmentResource} resource to convert
+   * @return The {@link CreateAssessmentCommand} command
+   */
+  public static CreateAssessmentCommand toCommandFromResource(CreateAssessmentResource resource) {
+    return new CreateAssessmentCommand(
+        resource.athleteId(),
+        resource.coachId(),
+        resource.bmi(),
+        resource.pushUps(),
+        resource.plankTime(),
+        resource.maxHeartRate(),
+        resource.restingHeartRate(),
+        resource.vo2max(),
+        resource.encryptedText(),
+        resource.type());
+  }
+}
