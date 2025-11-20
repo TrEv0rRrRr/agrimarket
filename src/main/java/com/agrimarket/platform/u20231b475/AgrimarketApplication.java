352835eca7c0d9657d2828195f1133b18e7e458e
Trevor <valentinosolischang@gmail.com>
2025-11-19 22:04:55 -0500

    Initialize agrimarket project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/agrimarket/platform/u20231b475/AgrimarketApplication.java
@@ -0,0 +1,13 @@
+package com.agrimarket.platform.u20231b475;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrimarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrimarket/platform/u20231b475/AgrimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrimarket.platform.u20231b475;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
