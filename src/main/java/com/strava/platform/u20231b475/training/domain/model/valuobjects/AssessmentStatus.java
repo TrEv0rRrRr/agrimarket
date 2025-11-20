b5eeb089f4930b40176f968a21bdf9712d7d88a6
Trevor <valentinosolischang@gmail.com>
2025-11-19 23:42:41 -0500

    Add AssessmentStatus enum for valid assessment states

---
+++ src/main/java/com/strava/platform/u20231b475/training/domain/model/valuobjects/AssessmentStatus.java
@@ -0,0 +1,12 @@
+package com.strava.platform.u20231b475.training.domain.model.valuobjects;
+
+/**
+ * Enum for the valid assessment status
+ * 
+ * @author Valentino Solis
+ */
+public enum AssessmentStatus {
+  RECORDED,
+  REVIEWED,
+  FLAGGED
+}
